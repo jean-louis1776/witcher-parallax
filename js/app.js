8da8a16a453333c0ab61cf345849c771bab3e14e
Ilya Aleksin <aleksin095@gmail.com>
2023-01-16 21:16:52 +0300

    add: new screens

--- js/app.js
+++ js/app.js
@@ -21,3 +21,16 @@ soundButton.addEventListener("click", (e) => {
 window.onfocus = function () {
   soundButton.classList.contains("paused") ? audio.pause() : audio.play()
 }
+
+// Scroll Reveal Animation
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  reset: false,
+})
+
+sr.reveal(".slider__logo_top", { delay: 800 })
+sr.reveal(`.slider__logo_middle`, { origin: "bottom" })
+sr.reveal(`.slider__logo_bottom`, { delay: 1000, origin: "bottom" })
